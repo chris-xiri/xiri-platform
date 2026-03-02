8f834c42ec7774472532f6ac3b971dda85839a65
chris-xiri <chris@xiri.ai>
2026-03-01 19:21:23 -0500

    fix: use /posts endpoint instead of /feed to exclude reels from FB posts feed

--- packages/functions/src/utils/facebookApi.ts
+++ packages/functions/src/utils/facebookApi.ts
@@ -303,7 +303,7 @@ export async function getRecentPosts(limit: number = 10): Promise<FacebookPost[]
     try {
         const fields = "id,message,created_time,full_picture,permalink_url,likes.summary(true),comments.summary(true),shares";
         const response = await fetch(
-            `${GRAPH_BASE_URL}/${PAGE_ID}/feed?fields=${fields}&limit=${limit}&access_token=${token}`
+            `${GRAPH_BASE_URL}/${PAGE_ID}/posts?fields=${fields}&limit=${limit}&access_token=${token}`
         );
 
         const data = await response.json();
